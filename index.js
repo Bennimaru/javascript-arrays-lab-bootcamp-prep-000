967f1e91da6f1352dbde0a686fddf080a40454db
Benson Yu <bennywhyu@gmail.com>
2018-09-10 22:26:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(ralph){
+  kittens.push(ralph)
+  return kittens
+}
+function destructivelyPrependKitten(ralph){
+  kittens.unshift(ralph)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(kittens,ralph){
+  kittens2 =kittens.concat(ralph)
+  return kittens2
+}
